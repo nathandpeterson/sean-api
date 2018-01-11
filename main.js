2fbbc9a7d0e1ad290e246b441d2b3f6e14f70b6a
Nathan Daniel Peterson <natperson@gmail.com>
2018-01-11 09:38:55 -0800

    add basic server

---
+++ main.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+const morgan = require('morgan')
+if (!process.env.NODE_ENV) app.use(morgan('dev'))
+
+const cors = require('cors')
+app.use(cors())
+app.disable('x-powered-by')
+
+app.use('/', (req, res, next) => {
+    res.json({message: 'hi'})
+})
+
+app.use((req, res, next) => {
+    const status = 404
+    const message = `Could not find route matching: ${req.method} ${req.path}`
+    next({ status, message })
+  })
+  
+  app.use((err, req, res, next) => {
+    if (!process.env.NODE_ENV) console.log(err)
+    const status = err.status || 500
+    const message = err.message || 'Something went wrong!'
+    res.status(status).json({ error: { message } })
+  })
+
+const listener = () => console.log(`listening on port ${port}`)
+app.listen(port, listener)
