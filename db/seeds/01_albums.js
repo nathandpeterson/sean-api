5652605f6efcbc568675e2c03a770015884f8929
Nathan Daniel Peterson <natperson@gmail.com>
2018-01-15 11:33:49 -0800

    set up to fetchSongs from AWS s3 bucket

--- db/seeds/01_albums.js
+++ db/seeds/01_albums.js
@@ -6,21 +6,21 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('albums').insert([
         { id: 1, 
-          name: 'cowboyhat sermons', 
-          artist: 'jonathan swift', 
-          imageURL: 'www.google.com',
+          name: 'owl and the rattlesnake', 
+          artist: 'ae oatley', 
+          imageURL: 'https://images.unsplash.com/photo-1496239298983-ebf973a467dc?auto=format&fit=crop&w=1050&q=80',
           description: 'an early effort'
         },
         { id: 2, 
-          name: 'smoof', 
-          artist: 'alexander the pop', 
-          imageURL: 'www.google.com',
+          name: 'cowboy hat sermons', 
+          artist: 'idiot love', 
+          imageURL: 'https://images.unsplash.com/photo-1499242165110-131f6ccd0c9a?auto=format&fit=crop&w=750&q=80',
           description: 'beats and heat'
         },
         { id: 3, 
           name: 'magic potato', 
           artist: 'william idiot', 
-          imageURL: 'www.google.com',
+          imageURL: 'https://images.unsplash.com/photo-1511441765914-190bdcb09e8a?auto=format&fit=crop&w=750&q=80',
           description: 'masterpiece theater'
         },
       ])
--- models/FetchSongs.js
+++ models/FetchSongs.js
@@ -1,8 +1,35 @@
-
+const AWS = require('aws-sdk')
+const s3 = new AWS.S3()
 
 class FetchSongs {
-    static one(id){
-        
+    static async test(req, res, next){
+        console.log(req.body.key)
+        await FetchSongs.getOne(req.body.key)
+            .then(result => {
+                return res.send(result)
+            })
+    }
+
+    static async getOne(songName){
+        let song = `test-songs/${songName}.mp3`
+       console.log('fetching this song', song)
+       await s3.getObject({Bucket: 'sean-songs', Key: song}, function(err, data) {
+            if (err)
+            console.log(err)
+            else
+            console.log(data.Body)
+            return data.Body
+        })
+    }
+    static download(id){
+
+        s3.fileDownload({Bucket: 'sean-songs', Key: 'test-songs/Hands.mp3'}, function(err, data) {
+            if (err)
+            console.log(err)
+            else
+            console.log(data.Body)
+            // res.send
+        })
     }
 }
 
