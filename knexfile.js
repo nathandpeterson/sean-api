fe461f8632dc1882fbb694dc568b9e2a235b36f1
Nathan Daniel Peterson <natperson@gmail.com>
2018-01-18 10:03:27 -0800

    configure knexfile for deployment

--- knexfile.js
+++ knexfile.js
@@ -16,6 +16,12 @@ module.exports = {
   production: {
     client: 'pg',
     connection: 'postgres://vpxsltcyvznnrj:0d37acc30a8d8f428be2957d3dfbfef17bf21f504d24961438837398a5242ec3@ec2-54-243-193-227.compute-1.amazonaws.com:5432/dhub0d1lkerr6',
+    migrations: {
+      directory: path.join(__dirname, 'db', 'migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'db', 'seeds')
+    }
   }
 
 }
